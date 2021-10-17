9faa40b45f9eb2c8f1f9be347b4ee7f579da543f
Fernando García <fernandgarcia97@hotmail.com>
2021-10-17 17:40:53 -0500

    Project to Netlify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { VueSpinners } from "@saeris/vue-spinners";
+import Chartick from "vue-chartkick";
+import Chart from "chart.js";
+import "@/assets/css/tailwind.css";
 
+import router from "@/router";
+import { dollarFilter, percentFilter } from "@/filters";
+
+Vue.use(VueSpinners);
+Vue.use(Chartick.use(Chart));
+Vue.filter("dollar", dollarFilter);
+Vue.filter("percent", percentFilter);
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
